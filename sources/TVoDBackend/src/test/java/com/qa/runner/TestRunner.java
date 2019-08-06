48d07f29ad5e498f11b3f44d15f843c6177ed672
thuytest <nguyenthuycn5@gmail.com>
2019-08-06 18:38:07 +0700

    config cucumber option

--- sources/TVoDBackend/src/test/java/com/qa/runner/TestRunner.java
+++ sources/TVoDBackend/src/test/java/com/qa/runner/TestRunner.java
@@ -13,7 +13,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 @CucumberOptions(features = "src/main/java/com/qa/features", glue = { "com.qa.stepDefinitions" }, tags = {
-		"~@Ignore" }, format = { "pretty", "html:target/cucumber-reports/cucumber-pretty",
+		"@SP19 and @SP20" }, format = { "pretty", "html:target/cucumber-reports/cucumber-pretty",
 				"json:target/cucumber-reports/CucumberTestReport.json", "rerun:target/cucumber-reports/rerun.txt" })
 
 public class TestRunner {
